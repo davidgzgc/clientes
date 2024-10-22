4a25d8827e1c30d04f8e251f13881c085a9d784b
kevinfranciscokf1 <43979489+kevinfranciscokf1@users.noreply.github.com>
2024-10-22 09:23:43 -0600

    Update Clientes_model.php
    Carga de la base de datos

--- application/models/Clientes_model.php
+++ application/models/Clientes_model.php
@@ -5,6 +5,7 @@ class Clientes_model extends CI_Model {
 
     public function __construct() {
         parent::__construct();
+		$this->load->database();
     }
 
     public function obtener_clientes() {
